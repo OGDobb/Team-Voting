5e0070af0ac9331a348885b84f4bf3230a2a366a
OGDobb <awolfe0009@mymail.lausd.net>
2022-01-15 19:22:38 -0800

    I added the firebase for the Team-Voting app along with changing the background color from coral to green

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyAg7EUn8xqycszSrHGYXy0N4tCvxECIKfU",
+    authDomain: "project-67-23b3c.firebaseapp.com",
+    databaseURL: "https://project-67-23b3c-default-rtdb.firebaseio.com",
+    projectId: "project-67-23b3c",
+    storageBucket: "project-67-23b3c.appspot.com",
+    messagingSenderId: "959917309922",
+    appId: "1:959917309922:web:1989852d012a5c09d903
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
